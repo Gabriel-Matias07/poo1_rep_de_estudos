6a7ae0dd1ce1126ff30e0ebe22836d0c78a89fd7
Gabriel-Matias07 <gabrielpereiramatias07@gmail.com>
2025-03-27 10:57:38 -0300

    organização

--- java_files/Main.java
+++ java_files/Main.java
@@ -1,10 +1,10 @@
-public class Main{
-    public static void main(String[]args) {
-            System.out.println("Hello, World!");
-            for (int i = 0; i < 5; i++){
-                System.out.println(i + 1);
-                System.out.println("Meu primeiro programa Java!");
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i + 1);
+            System.out.println("Meu primeiro programa Java!");
         }
-        }
+    }
 
-        }
+}
