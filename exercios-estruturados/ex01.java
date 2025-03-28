2e5ffe1a37973e33e3535559e6cdc995f2f9ee08
Gabriel-Matias07 <gabrielpereiramatias07@gmail.com>
2025-03-28 07:15:02 -0300

    printando resultados

--- exercios-estruturados/ex01.java
+++ exercios-estruturados/ex01.java
@@ -3,18 +3,20 @@ import java.util.Scanner;
 public class ex01 {
     public static void main(String[] args) {
         System.out.println("Digite uma temperatura em celsius: ");
+
 //        Declarando as variaveis
         double celsius = 0.0;
 
 //        Declarando as formulas
-
         double fahrenheit = (celsius * 1.8 + 32);
         double kelvin = (celsius + 273.15);
-
 
 //        Criando um objeto scanner
         Scanner scanner = new Scanner(System.in);
         double celsius = scanner.nextDouble();
+
+        System.out.printl(celsius "celsius em fahrenheit é: " + fahrenheit);
+        System.out.printl(celsius "celsius em kelvin é: " + kelvin);
 
 
     }
