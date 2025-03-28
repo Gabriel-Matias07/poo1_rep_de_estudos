77c2162da193b159a729d505cd82fc270d531b18
Gabriel-Matias07 <gabrielpereiramatias07@gmail.com>
2025-03-28 09:18:58 -0300

    att

--- exercios-estruturados/ex02.java
+++ exercios-estruturados/ex02.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Ex02{
     public static void main(String[] args) {
-        
+        int qtd_mulheres = 0;
+        // int qtd_homens = 0;
+        double altura_mulheres = 0;
+        double altura_homens = 0;
+        int sexo;
+
+        for (i = 5; i <= 5; i++)
     }
 }
