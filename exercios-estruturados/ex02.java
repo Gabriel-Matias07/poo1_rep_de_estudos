9c928d35659fca348b7413cce0839a68d116b882
Gabriel-Matias07 <gabrielpereiramatias07@gmail.com>
2025-03-28 09:44:06 -0300

    att

--- exercios-estruturados/ex02.java
+++ exercios-estruturados/ex02.java
@@ -3,25 +3,29 @@ import java.util.Scanner;
 public class Ex02{
     public static void main(String[] args) {
         int qtd_mulheres = 0;
-        // int qtd_homens = 0;
+        int qtd_homens = 0;
         double altura_mulheres = 0;
         double altura_homens = 0;
         int sexo;
 
-        for (i = 5; i < 5; i++){
+        for (int i = 0; i < 10; i++){
             System.out.println("Escolha o sexo da pessoa: (1 - Mulher, 2 - Homem): ");
             Scanner scanner = new Scanner(System.in);
             sexo = scanner.nextInt();
 
+            System.out.println("Digite a altura: ");
+            altura = scanner.nextDouble();
+
             if (sexo == 1){
                 qtd_mulheres++;
-            }
-            if (sexo == 2){
+            } else if (sexo == 2){
                 qtd_homens++;
             }
             else{
                 System.out.println("Entrada Inválida");
             }
+
+
         }
     }
 }
