7d57a6b114e093b08aa423a06efa8535c12526db
Gabriel-Matias07 <gabrielpereiramatias07@gmail.com>
2025-03-28 07:12:18 -0300

    definindo formulas e variaveis

--- exercios-estruturados/ex01.java
+++ exercios-estruturados/ex01.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class ex01 {
     public static void main(String[] args) {
         System.out.println("Digite uma temperatura em celsius: ");
-//        Declarando as variaveis e formulas
-            double celsius = 0.0;
-            double fahrenheit = 0.0;
-            double kelvin = 0.0;
+//        Declarando as variaveis
+        double celsius = 0.0;
+
+//        Declarando as formulas
+
+        double fahrenheit = (celsius * 1.8 + 32);
+        double kelvin = (celsius + 273.15);
 
 
 //        Criando um objeto scanner
