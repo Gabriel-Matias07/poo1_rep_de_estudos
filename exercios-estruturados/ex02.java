c1777917c9fabf2cebe2e321f4397821e4b10fe9
Gabriel-Matias07 <gabrielpereiramatias07@gmail.com>
2025-03-28 09:37:17 -0300

    att

--- exercios-estruturados/ex02.java
+++ exercios-estruturados/ex02.java
@@ -9,7 +9,19 @@ public class Ex02{
         int sexo;
 
         for (i = 5; i < 5; i++){
-            System.out.println("Escolha o sexo da pessoa: ()")
+            System.out.println("Escolha o sexo da pessoa: (1 - Mulher, 2 - Homem): ");
+            Scanner scanner = new Scanner(System.in);
+            sexo = scanner.nextInt();
+
+            if (sexo == 1){
+                qtd_mulheres++;
+            }
+            if (sexo == 2){
+                qtd_homens++;
+            }
+            else{
+                System.out.println("Entrada Inválida");
+            }
         }
     }
 }
