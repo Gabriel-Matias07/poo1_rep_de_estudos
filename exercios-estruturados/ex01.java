c3940a7933ad24ab6c2f005090e7c898ea5c1fe3
Gabriel-Matias07 <gabrielpereiramatias07@gmail.com>
2025-03-28 07:06:44 -0300

    att

--- exercios-estruturados/ex01.java
+++ exercios-estruturados/ex01.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class ex01 {
     public static void main(String[] args) {
+        System.out.println("Digite uma temperatura em celsius: ");
+//        Declarando as variaveis e formulas
+            double celsius = 0.0;
+            double fahrenheit = 0.0;
+            double kelvin = 0.0;
+
+
+//        Criando um objeto scanner
+        Scanner scanner = new Scanner(System.in);
+        double celsius = scanner.nextDouble();
+
 
     }
 }
