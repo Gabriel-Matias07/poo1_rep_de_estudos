7c0e26c04d973b13590cac9c498aac0a5a70251b
Gabriel-Matias07 <gabrielpereiramatias07@gmail.com>
2025-03-28 07:18:33 -0300

    corrigindo codigo

--- exercios-estruturados/ex01.java
+++ exercios-estruturados/ex01.java
@@ -4,19 +4,18 @@ public class ex01 {
     public static void main(String[] args) {
         System.out.println("Digite uma temperatura em celsius: ");
 
-//        Declarando as variaveis
-        double celsius = 0.0;
+
+//        Criando um objeto scanner e declarando variavel
+        Scanner scanner = new Scanner(System.in);
+        double celsius = scanner.nextDouble();
 
 //        Declarando as formulas
         double fahrenheit = (celsius * 1.8 + 32);
         double kelvin = (celsius + 273.15);
 
-//        Criando um objeto scanner
-        Scanner scanner = new Scanner(System.in);
-        double celsius = scanner.nextDouble();
 
-        System.out.printl(celsius "celsius em fahrenheit é: " + fahrenheit);
-        System.out.printl(celsius "celsius em kelvin é: " + kelvin);
+        System.out.println(celsius + "celsius em fahrenheit é: " + fahrenheit);
+        System.out.println(celsius + "celsius em kelvin é: " + kelvin);
 
 
     }
