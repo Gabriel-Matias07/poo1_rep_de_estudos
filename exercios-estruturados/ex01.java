09a116d2bef3c79ec95ee9262f8c7b9c8fb9d8a9
Gabriel-Matias07 <gabrielpereiramatias07@gmail.com>
2025-04-02 23:17:03 -0300

    comentarios removidos

--- exercios-estruturados/ex01.java
+++ exercios-estruturados/ex01.java
@@ -2,10 +2,8 @@ import java.util.Scanner;
 
 public class Ex01 {
     public static void main(String[] args) {
-        
         Scanner scanner = new Scanner(System.in);
 
-        // Solicitando a temperatura em Celsius
         System.out.print("Digite uma temperatura em Celsius: ");
         double celsius = scanner.nextDouble();
 
